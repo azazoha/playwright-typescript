32189ecdd9db3d081ea27f4adf9796fe31e73493
Zagir <azazoha@gmail.com>
2026-03-11 19:19:43 +0400

    add negative tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,4 +9,27 @@ test('user can login', async ({ page }) => {
   await page.click('[data-test="login-button"]');
 
   await expect(page).toHaveURL(/inventory/);
+  await expect(page.locator('.title')).toHaveText('Products');
+});
+
+test('wrong login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('wrong_secret');
+  
+  await page.click('[data-test="login-button"]');
+  
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
+test('locked out user', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  
+  await page.locator('[data-test="username"]').fill('locked_out_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
