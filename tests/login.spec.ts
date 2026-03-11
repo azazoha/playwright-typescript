2951a05487f036d20a6cad8c878ce021dd445ad3
Zagir <azazoha@gmail.com>
2026-03-11 19:21:42 +0400

    refactor login tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,34 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('user can login', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com');
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+});
 
-  await page.fill('[data-test="username"]', 'standard_user');
-  await page.fill('[data-test="password"]', 'secret_sauce');
-
-  await page.click('[data-test="login-button"]');
+test('successful login', async ({ page }) => {
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
 
   await expect(page).toHaveURL(/inventory/);
   await expect(page.locator('.title')).toHaveText('Products');
 });
 
-test('wrong login', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  
+test('wrong password', async ({ page }) => {
   await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('wrong_secret');
-  
-  await page.click('[data-test="login-button"]');
-  
+  await page.locator('[data-test="password"]').fill('wrong_password');
+  await page.locator('[data-test="login-button"]').click();
+
   await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
 
 test('locked out user', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  
   await page.locator('[data-test="username"]').fill('locked_out_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
-
   await page.locator('[data-test="login-button"]').click();
 
   await expect(page.locator('[data-test="error"]')).toBeVisible();
