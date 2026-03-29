d7454d3788d9e6f3392a5c489ae0985494ff8352
Zagir <azazoha@gmail.com>
2026-03-29 19:52:30 +0400

    tests: add cartPageWithItem fixture

--- fixtures/fixtures.ts
+++ fixtures/fixtures.ts
@@ -1,9 +1,13 @@
 import { test as base, Page } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
+import { InventoryPage } from '../pages/inventoryPage'
+import { CartPage } from '../pages/cartPage'
 import { users } from '../test-data/users';
+import { products } from '../test-data/products';
 
 type MyFixtures = {
     loggedInPage: Page;
+    cartPageWithItem: CartPage;
 }
 
 export const test = base.extend<MyFixtures>({
@@ -14,7 +18,17 @@ export const test = base.extend<MyFixtures>({
     await loginPage.login(users.standard.username, users.standard.password);
 
     await use(page);
+  },
+
+  cartPageWithItem: async ({ loggedInPage }, use) => {
+    const inventoryPage = new InventoryPage(loggedInPage);
+
+    await inventoryPage.addToCart(products.backpack);
+    await inventoryPage.openCart();
+
+    await use(new CartPage(loggedInPage));
   }
 });
+
 
 export { expect } from '@playwright/test';
